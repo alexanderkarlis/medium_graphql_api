2d6c503b5caab9b76fb79ec290081ee51542ec4c
Alexander Karlis <alexanderkarlis@gmail.com>
2019-11-09 20:37:24 -0500

    big commit

--- lib/medium_graphql_api/repo.ex
+++ lib/medium_graphql_api/repo.ex
@@ -2,4 +2,10 @@ defmodule MediumGraphqlApi.Repo do
   use Ecto.Repo,
     otp_app: :medium_graphql_api,
     adapter: Ecto.Adapters.Postgres
+
+    # def create_users(attrs \\ %{}) do
+    #   %User{}
+    #   |> User.changeset(attrs)
+    #   |> Repo.insert()
+    # end
 end
